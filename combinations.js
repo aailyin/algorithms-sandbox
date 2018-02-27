0ec4cbe03dfe31238f54b358916f911cebf3346c
Anton-Aleksei Ilyin <antonaleksei.ilyin@gmail.com>
2018-02-27 14:38:18 +0300

    added schema

--- combinations.js
+++ combinations.js
@@ -1,5 +1,14 @@
 // input: array like this [[1,2,3], [4,5], [6]]
 // output: [[1,4,6], [1,5,6], [2,4,6], [2,5,6], [3,4,6], [3,5,6]] - all combinations of elements
+//
+//         4 - 6
+//       /
+//     1 - 5 - 6
+//   /   
+// * - 2 - // the same tree as for 1  
+//   \ 
+//     3 - // the same tree as for 1
+//
 
 (function (){
     function getElements(arr) {
