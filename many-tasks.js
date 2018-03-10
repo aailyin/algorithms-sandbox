b909edea06020c685628aeb786437a0b2b2469db
Anton-Aleksei Ilyin <antonaleksei.ilyin@gmail.com>
2018-03-10 18:45:20 +0300

    added palindrome check

--- many-tasks.js
+++ many-tasks.js
@@ -19,3 +19,47 @@ var singleNumber = function(nums) {
         }
     }
 };
+
+
+/* Task 2
+ * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ *
+ * For example,
+ * "A man, a plan, a canal: Panama" is a palindrome.
+ * "race a car" is not a palindrome.
+ *
+ * Note:
+ * Have you consider that the string might be empty? This is a good question to ask during an interview.
+ * 
+ * For the purpose of this problem, we define empty string as valid palindrome.
+ *
+ * Solution:
+ *
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    if (!s) { return true; }
+    
+    var arr = [], length = s.length, i = 0, length2, charcode;
+    for (i; i < length; i++) {
+        charcode = s[i].charCodeAt(0);
+        if ((charcode >= 97 && charcode <= 122) || (charcode >= 65 && charcode <= 90) || (charcode >= 48 && charcode <= 57)) {
+            arr.push(s[i].toLowerCase());
+        }
+    }
+    length = arr.length;
+    
+    if (length % 2 == 0) { 
+        length2 = length/2; 
+    } else {
+        length2 = (length - 1)/2
+    }
+    
+    for (i = 0; i < length2; i++) {
+        if (arr[i] !== arr[length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+};
