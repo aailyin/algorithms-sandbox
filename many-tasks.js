f43ac1ed45c10fbd2a7c1f505bd39798b3107a8c
Anton-Aleksei Ilyin <antonaleksei.ilyin@gmail.com>
2018-03-12 23:40:07 +0300

    added restore ip addresses

--- many-tasks.js
+++ many-tasks.js
@@ -74,3 +74,41 @@ For example:
 Given "25525511135",
 return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
 */
+/**
+ * @param {string} str
+ * @return {string[]}
+ */
+var restoreIpAddresses = function(str) {
+    const result = [];
+        const length = str.length;
+        
+        check(0, 0, '');
+
+        function check(start, level, previous){
+            let i = 0;
+            let num;
+
+            if (level === 3) {
+                num = str.substring(start);
+                if (num[0] == '0' && num.length > 1) {
+                    return;
+                }
+                if (num && num < 256) {
+                    result.push(`${previous}.${num}`);
+                }
+                return;                
+            }
+            num = str.substring(start, start + 1);
+            if (parseInt(num, 10) == 0) {
+                check(start + 1, level + 1, level === 0 ? `${num}`: `${previous}.${num}`);
+            } else {
+                while (num.length < 4 && num < 256 && start + i + 1 < length) {
+                    check(start + i + 1, level + 1, level === 0 ? `${num}`: `${previous}.${num}`);
+                    i++;
+                    num = str.substring(start, start + i + 1);
+                }
+            }
+        }
+
+        return result;
+};
