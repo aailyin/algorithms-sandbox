7f523b6ba87d8dcd4c6d7158ce5773b8345355a4
Anton-Aleksei Ilyin <antonaleksei.ilyin@gmail.com>
2018-03-12 23:09:38 +0300

    Update many-tasks.js

--- many-tasks.js
+++ many-tasks.js
@@ -1,3 +1,5 @@
+// LeetCode tasks:
+
 /* Task 1
  * Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
  * Note:
@@ -63,3 +65,12 @@ var isPalindrome = function(s) {
     }
     return true;
 };
+
+/*
+https://leetcode.com/problems/restore-ip-addresses/description/
+
+Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+For example:
+Given "25525511135",
+return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+*/
